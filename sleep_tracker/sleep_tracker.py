ef2b487b63ff48f1ae9e2ff3c4693d3a24b1adf5
Philip Holy <philipmholy@gmail.com>
2020-02-10 22:07:44 -0700

    Initial setup

---
+++ sleep_tracker/sleep_tracker.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+from flask import Flask, g, render_template
+
+DB_PATH = "/var/sync/tracker.db"
+
+app = Flask(__name__)
+
+
+def open_connection():
+    connection = getattr(g, "_connection", None)
+    if connection is None:
+        connection = g._connection = sqlite3.connect(DB_PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit is True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, "_connection", None)
+    if connection is not None:
+        connection.close()
+
+
+@app.route("/")
+def home():
+    tracking_types = execute_sql("select tracker_type_code from tracker_type")
+    return render_template("index.html", tracking_types=tracking_types)
